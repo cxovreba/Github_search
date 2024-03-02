4dd96512df6f26666965e245ece2b09fec1e9051
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-03-02 17:40:20 +0400

    final changes

--- app.js
+++ app.js
@@ -72,95 +72,81 @@ mode_change.addEventListener("click", () => {
   applyStyles(!currentMode);
 });
 
-const search_input = document.getElementById("search_input");
-const search__btn = document.getElementById("search_btn");
+const search_btn = document.getElementById("search_btn");
 
-search__btn.addEventListener("click", () => {
-  let loader = `<span class="loader"></span>`;
-  document.querySelector(".result").innerHTML = loader;
-
-  searchResault();
-
-  search_input.value = "";
+search_btn.addEventListener("click", () => {
+  const search_input = document.getElementById("search_input");
+  const search_value = search_input.value.trim();
+  if (search_value !== "") {
+    searchResault(search_value);
+    search_input.value = "";
+  };
+  
 });
 
-const searchResault = () => {
-  const search_value = search_input.value;
+const searchResault = (search_value) => {
+  const content_container = document.querySelector(".result");
+  let loader = `<span class="loader"></span>`;
+  content_container.innerHTML = loader;
 
   try {
     fetch(`https://api.github.com/users/${search_value}`)
       .then((response) => response.json())
       .then((data) => {
-        const content_container = document.querySelector(".result");
-
-        content_container.innerHTML = `
-                    <div class="profile_img1">
-                        <img class="img1" src="${
-                          data.avatar_url
-                        }" alt="" style="width: 117px; height: 117px; border-radius: 117px">
-                    </div>
-                    <div class="result_container">
-                        <div class="profile_info">
-                            <div class="profile_img">
-                                <img class="img" src="${
-                                  data.avatar_url
-                                }" alt="">
-                            </div>
-                            <div class="username_info">
-                                <div class="info">
-                                    <div class="username">${data.login}</div>
-                                    <div class="name">${data.name}</div>
-                                </div>
-                                <div class="join_date">joined: ${new Date(
-                                  data.created_at
-                                ).toDateString()}</div>
-                            </div>
-                        </div>
-                        <div class="details">${
-                          data.bio || "Not available"
-                        }</div>
-                        <div class="follow_info">
-                            <div class="repository_quantity">
-                                <div class="unit">Repos</div>
-                                <div class="quantity">${data.public_repos}</div>
-                            </div>
-                            <div class="follower_quantity">
-                                <div class="unit">Followers</div>
-                                <div class="quantity">${data.followers}</div>
-                            </div>
-                            <div class="following_quantity">
-                                <div class="unit">Following</div>
-                                <div class="quantity">${data.following}</div>
-                            </div>
-                        </div>
-                        <div class="links">
-                            <div class="location_tag">
-                                <img class="link_icon" src="./icons/003-pin.png" alt="">
-                                <a class="location" href="#">${
-                                  data.location || "Not available"
-                                }</a>
-                            </div>
-                            <div class="github_tag">
-                                <img class="link_icon" src="./icons/002-url.png" alt="">
-                                <a class="github_link" href="">${
-                                  data.html_url || "Not available"
-                                }</a>
-                            </div>
-                            <div class="twiter_tag">
-                                <img class="link_icon" src="./icons/004-twitter.png" alt="">
-                                <a class="twiter" href="">${
-                                  data.twitter_username || "Not available"
-                                }</a>
-                            </div>
-                            <div class="office_tag">
-                                <img class="link_icon" src="./icons/001-office-building.png" alt="">
-                                <a class="office" href="">${
-                                  data.company || "Not available"
-                                }</a>
-                            </div>
-                        </div>
-                    </div>`;
-        // document.querySelector('.result').innerHTML= content_container;
+        
+        const content = `
+          <div class="profile_img1">
+            <img class="img1" src="${data.avatar_url}" alt="" style="width: 117px; height: 117px; border-radius: 117px">
+          </div>
+          <div class="result_container">
+              <div class="profile_info">
+                  <div class="profile_img">
+                      <img class="img" src="${data.avatar_url}" alt="">
+                  </div>
+                  <div class="username_info">
+                      <div class="info">
+                          <div class="username">${data.login}</div>
+                          <div class="name">${data.name}</div>
+                      </div>
+                      <div class="join_date">joined: ${new Date(data.created_at).toDateString()}</div>
+                  </div>
+              </div>
+              <div class="details">${data.bio || "Not available"}</div>
+              <div class="follow_info">
+                  <div class="repository_quantity">
+                      <div class="unit">Repos</div>
+                      <div class="quantity">${data.public_repos}</div>
+                  </div>
+                  <div class="follower_quantity">
+                      <div class="unit">Followers</div>
+                      <div class="quantity">${data.followers}</div>
+                  </div>
+                  <div class="following_quantity">
+                      <div class="unit">Following</div>
+                      <div class="quantity">${data.following}</div>
+                  </div>
+              </div>
+              <div class="links">
+                  <div class="location_tag">
+                      <img class="link_icon" src="./icons/003-pin.png" alt="">
+                      <a class="location" href="#">${data.location || "Not available"}</a>
+                  </div>
+                  <div class="github_tag">
+                      <img class="link_icon" src="./icons/002-url.png" alt="">
+                      <a class="github_link" href="">${data.html_url || "Not available"}</a>
+                  </div>
+                  <div class="twiter_tag">
+                      <img class="link_icon" src="./icons/004-twitter.png" alt="">
+                      <a class="twiter" href="">${data.twitter_username || "Not available"}</a>
+                  </div>
+                  <div class="office_tag">
+                      <img class="link_icon" src="./icons/001-office-building.png" alt="">
+                      <a class="office" href="">${data.company || "Not available"}</a>
+                  </div>
+              </div>
+          </div>`;
+          content_container.innerHTML = content;
+          applyStyles(document.body.classList.contains("mode_change"));
       });
   } catch (error) {
     alert("No results");
